28a5c4a9061fcc4b39ffa229bf0083dca4e8bb84
AlanaKaimi <ajbryce@live.com>
2019-06-25 17:41:54 -0400

    Drum Kit up and working

--- drums.js
+++ drums.js
@@ -1,9 +1,19 @@
-window.addEventListener('keydown', function (e) {
+function playSound (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
   if (!audio) return // stops function from running if there's no keyCode associated with it
   audio.currentTime = 0 // rewind to the start so audio can be played in quick succesion.
   audio.play()
-  console.log(key)
-})
+  key.classList.add('playing')
+}
+
+function removeTransition (e) {
+  if (e.propertyName !== 'transform') return // skip if not a transform
+  this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playSound)
